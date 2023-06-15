a4df129a8115f4ba62082162f97ab8b6c921e397
Martin Pospisil <martin.pospisil@ideastatica.com>
2023-06-15 23:56:53 +0200

    mfc - working locally but not in the container

---
+++ Examples/CppNativeAppTst/ClrClassLibrary/MyService.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace ClrClassLibrary {
+	public ref class MyService
+	{
+	public:
+		int Add(int a, int b);
+	};
+}
--- Examples/CppNativeAppTst/MyConsole/Program.cs
+++ Examples/CppNativeAppTst/MyConsole/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+
 Console.WriteLine("Hello, World!");
+
+var service = new ClrClassLibrary.MyService();
+var res = service.Add(2, 3);
+Console.WriteLine($"Result is {res}");
